6cd31ee6f8e12711f05348490eee6e4c183d5cac
Sabatier Audric <100680489+Audric-S@users.noreply.github.com>
2024-10-25 15:25:56 +0200

    Update app.py

--- testAPIFInance/app.py
+++ testAPIFInance/app.py
@@ -2,10 +2,175 @@ import streamlit as st
 import yfinance as yf
 import pydeck as pdk
 import pandas as pd
-import numpy as np
 
-# Chargement des données des entreprises à partir du CSV
-companies_df = pd.read_csv("data.csv")  # Assurez-vous que le chemin est correct
+# Données des entreprises S&P 500 intégrées
+data = {
+    "Name": [
+        "Apple Inc", "Microsoft Corp", "Amazon.com Inc", "Alphabet Inc Class A", 
+        "Alphabet Inc Class C", "Berkshire Hathaway Inc", "Meta Platforms Inc", 
+        "Tesla Inc", "NVIDIA Corp", "JPMorgan Chase & Co", "Visa Inc", "UnitedHealth Group Inc", 
+        "Johnson & Johnson", "Walmart Inc", "Procter & Gamble Co", "Mastercard Inc", 
+        "Home Depot Inc", "Disney (Walt) Co", "Salesforce.com Inc", "Verizon Communications Inc", 
+        "Coca-Cola Co", "Pfizer Inc", "Cisco Systems Inc", "PepsiCo Inc", "Exxon Mobil Corp",
+        "Chevron Corp", "AbbVie Inc", "Netflix Inc", "Intel Corp", "Amgen Inc",
+        "Nexstar Media Group Inc", "Union Pacific Corp", "Medtronic plc", "3M Co", 
+        "Boeing Co", "Starbucks Corp", "PayPal Holdings Inc", "Lockheed Martin Corp", 
+        "Thermo Fisher Scientific Inc", "IBM Corp", "T-Mobile US Inc", "Qualcomm Inc",
+        "General Electric Co", "AT&T Inc", "Caterpillar Inc", "Texas Instruments Inc",
+        "Salesforce Inc", "American Tower Corp", "S&P Global Inc", "Booking Holdings Inc",
+        "Analog Devices Inc", "General Motors Co", "Broadcom Inc", "Bristol Myers Squibb Co",
+        "Honeywell International Inc", "Linde plc", "Charter Communications Inc", "Equinix Inc",
+        "Duke Energy Corp", "Illinois Tool Works Inc", "Northrop Grumman Corp", "Activision Blizzard Inc",
+        "Capital One Financial Corp", "Southern Co", "Gilead Sciences Inc", "Biogen Inc",
+        "Vertex Pharmaceuticals Inc", "Lam Research Corp", "Walt Disney Co", "American Express Co",
+        "HCA Healthcare Inc", "Regeneron Pharmaceuticals Inc", "Crown Castle Inc", "CSX Corp",
+        "Kweichow Moutai Co Ltd", "Cintas Corp", "The Kraft Heinz Co", "Freeport-McMoRan Inc",
+        "Marriott International Inc", "Royal Caribbean Group", "Altria Group Inc", "NextEra Energy Inc",
+        "KeyCorp", "Kroger Co", "Parker-Hannifin Corp", "Stryker Corp",
+        "Hewlett Packard Enterprise Co", "Dell Technologies Inc", "D.R. Horton Inc", "PulteGroup Inc",
+        "Foot Locker Inc", "Kellogg Co", "Mosaic Co", "Lennar Corp",
+        "Newmont Corp", "Bristol-Myers Squibb Company", "Moderna Inc", "Zebra Technologies Corp",
+        "EOG Resources Inc", "Simon Property Group Inc", "Aflac Inc", "Fortinet Inc",
+        "BlackRock Inc", "American Airlines Group Inc", "Chubb Ltd", "Analog Devices Inc",
+        "FISV", "Synchrony Financial", "Tapestry Inc", "MGM Resorts International",
+        "Humana Inc", "CME Group Inc", "NortonLifeLock Inc", "Baker Hughes Co",
+        "Huntington Bancshares Inc", "Vornado Realty Trust", "Boston Scientific Corp", 
+        "Workday Inc", "PPL Corp", "Clorox Co", "SBA Communications Corp",
+        "Xilinx Inc", "Noble Energy Inc", "Eversource Energy", "Williams Companies Inc",
+        "Intercontinental Exchange Inc", "Lennar Corp", "Quest Diagnostics Inc", "FMC Corp",
+        "Sempra Energy", "Ryder System Inc", "Weyerhaeuser Co", "Marsh & McLennan Companies Inc",
+        "Carlyle Group Inc", "DuPont de Nemours Inc", "Snap Inc", "Regeneron Pharmaceuticals Inc",
+        "Western Digital Corp", "Biogen Inc", "Harris Corp", "Darden Restaurants Inc",
+        "Centrica plc", "Arista Networks Inc", "Seagate Technology Holdings plc", "Zillow Group Inc",
+        "Brighthouse Financial Inc", "Spirit AeroSystems Holdings Inc", "Newell Brands Inc", 
+        "WABCO Holdings Inc", "Maxim Integrated Products Inc", "Broadridge Financial Solutions Inc",
+        "International Flavors & Fragrances Inc", "L3Harris Technologies Inc", "Molina Healthcare Inc",
+        "Hewlett Packard Enterprise Co", "Cimpress plc", "Cheniere Energy Inc", "International Paper Co",
+        "Electronic Arts Inc", "Northwest Natural Holding Co", "Verisk Analytics Inc", "Marathon Petroleum Corp",
+        "Keurig Dr Pepper Inc", "KBR Inc", "Fastenal Co", "L3Harris Technologies Inc",
+        "PPG Industries Inc", "CoStar Group Inc", "D.R. Horton Inc", "Wolverine World Wide Inc",
+        "Salesforce.com Inc", "Fastenal Co", "Keysight Technologies Inc", "Bristol-Myers Squibb Co",
+        "Pfizer Inc", "Discover Financial Services", "ViacomCBS Inc", "Baxter International Inc",
+        "Coterra Energy Inc", "Kraft Heinz Co", "Hanesbrands Inc", "ViacomCBS Inc",
+        "Corning Inc", "Chubb Limited", "Omnicom Group Inc", "SS&C Technologies Holdings Inc",
+        "Truist Financial Corp", "First Republic Bank", "PG&E Corp", "Hanesbrands Inc",
+        "D.R. Horton Inc", "Dexcom Inc", "Cameron International Corp", "Lennar Corp",
+        "Motorola Solutions Inc", "Navient Corp", "Spectrum Brands Holdings Inc", "Citizens Financial Group Inc",
+        "Prologis Inc", "Ameriprise Financial Inc", "Nordstrom Inc", "Las Vegas Sands Corp",
+        "Seagate Technology Holdings plc", "Ulta Beauty Inc", "MGM Resorts International", "Intercontinental Exchange Inc",
+        "AECOM", "S&P Global Inc", "Cognizant Technology Solutions Corp", "Evercore Inc",
+        "Skyworks Solutions Inc", "McKesson Corp", "Seagate Technology Holdings plc", "Hanesbrands Inc"
+    ],
+    "Ticker": [
+        "AAPL", "MSFT", "AMZN", "GOOGL", 
+        "GOOG", "BRK-B", "META", 
+        "TSLA", "NVDA", "JPM", "V", "UNH", 
+        "JNJ", "WMT", "PG", "MA", 
+        "HD", "DIS", "CRM", "VZ", 
+        "KO", "PFE", "CSCO", "PEP", "XOM",
+        "CVX", "ABBV", "NFLX", "INTC", "AMGN",
+        "NXST", "UNP", "MDT", "MMM", 
+        "BA", "SBUX", "PYPL", "LMT", 
+        "TMO", "IBM", "TMUS", "QCOM",
+        "GE", "T", "CAT", "TXN",
+        "NOW", "AMT", "SPGI", "BKNG",
+        "ADI", "GM", "AVGO", "BMY",
+        "HON", "LIN", "CHTR", "EQIX",
+        "DUK", "ITW", "NOC", "ATVI",
+        "COF", "SO", "GILD", "BIIB",
+        "VRTX", "LRCX", "DIS", "AXP",
+        "HCA", "REGN", "CCI", "CSX",
+        "Kweichow Moutai Co Ltd", "CTAS", "KHC", "FCX",
+        "MAR", "RCL", "MO", "NEE",
+        "KEY", "KR", "PH", "SYK",
+        "HPE", "DELL", "DHI", "PHM",
+        "FL", "K", "MOS", "LEN",
+        "NEM", "BMY", "MRNA", "ZBRA",
+        "EOG", "SPG", "AFL", "FTNT",
+        "BLK", "AAL", "CB", "ADI",
+        "FISV", "SYF", "TPR", "MGM",
+        "HUM", "CME", "NLOK", "BKR",
+        "HBAN", "VNO", "BSX", 
+        "WDAY", "PPL", "CLX", "SBAC",
+        "XLNX", "NBL", "ES", "WMB",
+        "ICE", "LEN", "DGX", "FMC",
+        "SRE", "R", "WY", "MMC",
+        "CG", "DD", "SNAP", "REGN",
+        "WDC", "BIIB", "HRS", "DRI",
+        "CNA", "OMC", "SSNC", "TFC",
+        "FRC", "PCG", "HBI", "VIAC",
+        "GLW", "CB", "OMC", "SSNC",
+        "TFC", "FRC", "PCG", "HBI",
+        "DHI", "DXCM", "CAM", "LEN",
+        "MSI", "NAVI", "SPB", "CZR",
+        "PLD", "AMP", "JWN", "LVS",
+        "STX", "ULTA", "MGM", "ICE",
+        "ACM", "SPGI", "CTSH", "EVR",
+        "SWKS", "MCK", "STX", "HBI"
+    ],
+    "Latitude": [
+        37.3349, 47.6396, 47.6062, 37.4220, 
+        37.4220, 41.2623, 37.4848, 
+        37.3947, 37.3695, 40.7566, 37.7749, 44.9778,
+        40.7128, 36.1627, 39.0997, 34.0522,
+        33.7490, 33.6846, 37.7749, 40.7128,
+        33.4484, 40.7128, 37.7749, 33.4484, 29.7604,
+        29.9511, 41.8781, 38.9072, 41.8781, 37.7749,
+        39.7392, 38.6270, 37.7749, 39.7392,
+        37.7749, 39.7392, 36.1627, 40.7128,
+        40.7128, 34.0522, 36.1627, 39.0997,
+        38.9072, 39.7392, 38.6270, 34.0522,
+        37.7749, 41.8781, 36.1627, 37.7749,
+        34.0522, 34.0522, 39.7392, 33.4484,
+        33.4484, 39.7392, 33.4484, 34.0522,
+        39.7392, 34.0522, 34.0522, 33.4484,
+        34.0522, 34.0522, 36.1627, 34.0522,
+        39.7392, 37.7749, 36.1627, 41.8781,
+        39.7392, 34.0522, 34.0522, 34.0522,
+        39.7392, 37.7749, 36.1627, 34.0522,
+        34.0522, 36.1627, 39.7392, 37.7749,
+        34.0522, 39.7392, 41.8781, 34.0522,
+        39.7392, 39.7392, 34.0522, 37.7749,
+        36.1627, 34.0522, 39.7392, 39.7392,
+        41.8781, 39.7392, 37.7749, 37.7749,
+        39.7392, 39.7392, 39.7392, 39.7392,
+        41.8781, 39.7392, 41.8781, 39.7392,
+        36.1627, 39.7392, 39.7392, 34.0522,
+        41.8781, 36.1627, 39.7392, 34.0522
+    ],
+    "Longitude": [
+        -122.0090, -122.1281, -122.3321, -122.0841, 
+        -122.0841, -95.9378, -122.1484, 
+        -122.1500, -122.0365, -73.9884, -122.4194, -93.2650,
+        -74.0060, -86.7816, -94.5783, -118.2437,
+        -84.3879, -117.1611, -122.4194, -74.0060,
+        -112.0740, -74.0060, -122.4194, -112.0740, -95.3698,
+        -95.3698, -87.6298, -77.0369, -87.6298, -122.4194,
+        -104.9903, -90.1994, -122.4194, -104.9903,
+        -122.4194, -104.9903, -86.7816, -74.0060,
+        -74.0060, -118.2437, -86.7816, -94.5783,
+        -77.0369, -104.9903, -87.6298, -118.2437,
+        -122.4194, -73.9352, -86.7816, -122.4194,
+        -73.9352, -73.9352, -74.0060, -122.4194,
+        -77.0369, -77.0369, -77.0369, -74.0060,
+        -112.0740, -77.0369, -112.0740, -118.2437,
+        -112.0740, -104.9903, -104.9903, -112.0740,
+        -118.2437, -122.4194, -104.9903, -74.0060,
+        -73.9352, -73.9352, -104.9903, -77.0369,
+        -122.4194, -122.4194, -104.9903, -104.9903,
+        -104.9903, -122.4194, -122.4194, -104.9903,
+        -104.9903, -104.9903, -104.9903, -112.0740,
+        -112.0740, -112.0740, -104.9903, -104.9903,
+        -104.9903, -122.4194, -122.4194, -112.0740,
+        -112.0740, -112.0740, -118.2437, -118.2437,
+        -118.2437, -104.9903, -104.9903, -118.2437,
+        -104.9903, -104.9903, -104.9903, -104.9903,
+        -104.9903, -104.9903, -104.9903, -104.9903
+    ]
+}
+
+# Création d'un DataFrame à partir des données
+companies_df = pd.DataFrame(data)
 
 # DataFrame pour stocker les données des emplacements
 locations_df = []
@@ -37,62 +202,84 @@ for index, row in companies_df.iterrows():
             height = market_cap * 100  # Multiplier par 100 pour une meilleure visualisation
             
             # Largeur : revenus
-            width = revenue  # Utiliser les revenus directement pour la largeur
-
-            # Couleur basée sur les revenus
-            color = revenue_to_color(revenue)
-
-            # Ajouter les données de l'entreprise au DataFrame
+            width = revenue / 10  # Diviser par 10 pour la visualisation
+            
+            # Créer une entrée pour le cylindre
             locations_df.append({
-                "name": row['Name'],
-                "market_cap": f"{market_cap:.1f}B",  # Formater la capitalisation boursière
-                "revenue": f"{revenue:.1f}B",  # Revenus formatés
-                "net_income": f"{net_income:.1f}B",  # Bénéfice net formaté
-                "pe_ratio": pe_ratio,  # Ratio Cours/Bénéfice
-                "inception": inception,  # Année de création
-                "latitude": row['Latitude'],
-                "longitude": row['Longitude'],
-                "elevation": height,  # Hauteur basée sur la capitalisation boursière
-                "width": width,  # Largeur basée sur les revenus
-                "color": color
+                'Name': row['Name'],
+                'Ticker': row['Ticker'],
+                'Latitude': row['Latitude'],
+                'Longitude': row['Longitude'],
+                'Height': height,
+                'Width': width,
+                'MarketCap': market_cap,
+                'Revenue': revenue,
+                'NetIncome': net_income,
+                'PERatio': pe_ratio,
+                'Inception': inception,
+                'Color': revenue_to_color(revenue)
             })
     except Exception as e:
-        st.warning(f"Could not retrieve data for {row['Ticker']}: {e}")
+        print(f"Erreur avec {row['Name']}: {e}")
 
-# Convertir le DataFrame en DataFrame pandas pour Pydeck
+# Convertir les emplacements en DataFrame
 locations_df = pd.DataFrame(locations_df)
 
-# Créer une couche 3D pour la carte
+# Définir la vue initiale de la carte
+initial_view_state = pdk.ViewState(
+    latitude=37.0902,
+    longitude=-95.7129,
+    zoom=3,
+    pitch=0,
+)
+
+# Créer les objets pour les cylindre
 layer = pdk.Layer(
     "ColumnLayer",
     data=locations_df,
-    get_position="[longitude, latitude]",
-    get_elevation="elevation",
-    elevation_scale=1,  # Ajustez ceci si nécessaire
-    radius=20000,  # Réduire la largeur des colonnes
-    get_fill_color="color",
+    get_position=["Longitude", "Latitude"],
+    get_radius="Width",
+    get_height="Height",
+    get_fill_color="Color",
+    elevation_scale=10,
     pickable=True,
-    auto_highlight=True,
 )
 
-# Configuration de la vue de la carte
-view_state = pdk.ViewState(
-    latitude=39.5,
-    longitude=-98.35,
-    zoom=3,
-    pitch=45,
-)
-
-# Afficher la carte avec un info-bulle enrichi
+# Configuration de la carte
 deck = pdk.Deck(
-    map_style="mapbox://styles/mapbox/light-v9",
-    initial_view_state=view_state,
     layers=[layer],
-    tooltip={
-        "html": "<b>{name}</b><br/>Market Cap: {market_cap}<br/>Revenue: {revenue}<br/>Net Income: {net_income}<br/>P/E Ratio: {pe_ratio}<br/>Year Founded: {inception}",
-        "style": {"color": "white"},
-    },
+    initial_view_state=initial_view_state,
+    tooltip={"text": "{Name}"},
 )
 
-# Afficher la carte dans Streamlit
+# Titre de l'application
+st.title("Visualisation des entreprises du S&P 500")
+
+# Affichage de la carte
 st.pydeck_chart(deck)
+
+# Zone de détails
+if 'info' not in st.session_state:
+    st.session_state.info = ""
+
+# Affichage des détails des entreprises
+if st.session_state.info:
+    st.write("Détails de l'entreprise sélectionnée:")
+    st.write(st.session_state.info)
+
+# Callback pour récupérer les données de l'entreprise
+def on_click(event):
+    company = locations_df.iloc[event.index]
+    details = f"""
+    **Nom:** {company['Name']}
+    **Ticker:** {company['Ticker']}
+    **Market Cap:** ${company['MarketCap']:.2f} milliards
+    **Revenus:** ${company['Revenue']:.2f} milliards
+    **Bénéfice Net:** ${company['NetIncome']:.2f} milliards
+    **Ratio Cours/Bénéfice:** {company['PERatio']}
+    **Année de création:** {company['Inception']}
+    """
+    st.session_state.info = details
+
+# Ajouter le callback à la couche
+deck.on_click(on_click)
